e16a8412ca8af1b0b46055d8ffbd72b1e2e044b3
petrovaliev95 <petrovaliev95@abv.bg>
2015-05-02 16:33:49 +0300

    New features

--- MVC/DefaultController.php
+++ MVC/DefaultController.php
@@ -14,38 +14,37 @@
 namespace MVC;
 
 class DefaultController {
-    
-    /**
-     *
-     * @var \MVC\App 
-     */
-    public $app;
-    /**
-     *
-     * @var \MVC\View
-     */
-    public $view;
-    /**
-     *
-     * @var \MVC\Config 
-     */
-    public $config;
-    /**
-     *
-     * @var \MVC\InputData 
-     */
-    public $input;
 
-    public function __construct() {
-        $this->app = \MVC\App::getInstance();
-        $this->view = \MVC\View::getInstance();
-        $this->config = $this->app->getConfig();
-        $this->input = \MVC\InputData::getInstance();
-    }
-    
-    public function jsonResponse(){
-        
-    }
+	/**
+	 *
+	 * @var \MVC\App
+	 */
+	public $app;
+	/**
+	 *
+	 * @var \MVC\View
+	 */
+	public $view;
+	/**
+	 *
+	 * @var \MVC\Config
+	 */
+	public $config;
+	/**
+	 *
+	 * @var \MVC\InputData
+	 */
+	public $input;
+
+	public function __construct() {
+		$this->app = \MVC\App::getInstance();
+		$this->view = \MVC\View::getInstance();
+		$this->config = $this->app->getConfig();
+		$this->input = \MVC\InputData::getInstance();
+	}
+
+	public function jsonResponse() {
+
+	}
 
 }
-
--- MVC/View.php
+++ MVC/View.php
@@ -12,104 +12,119 @@
  */
 namespace MVC;
 class View {
-    
-    private static $_instance = null;
-    private $___viewPath = null;
-    private $___viewDir = null;
-    private $___data = array();
-    private $___extension = '.php';
-    private $___layoutParts = array();
-    private $___layoutData=array();
-    private function __construct() {        
-        
-        $this->___viewPath = \MVC\App::getInstance()->getConfig()->app['viewsDirectory'];
-        if ($this->___viewPath == null) {
-            $this->___viewPath = realpath('../views/');
-        }
-    }
-    
-    public function setViewDirectory($path) {
-        $path = trim($path);
-        if ($path) {
-            $path = realpath($path) . DIRECTORY_SEPARATOR;
-            if (is_dir($path) && is_readable($path)) {
-                $this->___viewDir = $path;
-            } else {
-                //todo
-                throw new \Exception('view path',500);
-            }
-        } else {
-            //todo
-            throw new \Exception('view path',500);
-        }
-    }
-    
-     public function display($name, $data = array(), $returnAsString = false) {
 
-        if (is_array($data)) {
-            $this->___data = array_merge($this->___data, $data);
-        }
-        
-        if (count($this->___layoutParts) > 0) {
-            foreach ($this->___layoutParts as $k => $v) {
-                $r = $this->_includeFile($v);
-                if ($r) {
-                    $this->___layoutData[$k] = $r;
-                }
-            }
-        }
-        
-        if ($returnAsString) {
-            return $this->_includeFile($name);
-        } else {
-            echo $this->_includeFile($name);
-        }
-    }
-    
-    public function getLayoutData($name){
-        return $this->___layoutData[$name];
-    }
-    
-    public function appendToLayout($key, $template) {
-        if ($key && $template) {
-            $this->___layoutParts[$key] = $template;
-        } else {
-            throw new \Exception('Layout ruqire valid key and tepmplate', 500);
-        }
-    }
-    private function _includeFile($file) {
-        if ($this->___viewDir == null) {
-            $this->setViewDirectory($this->___viewPath);
-        }       
-        $___fl = $this->___viewDir . str_replace('.', DIRECTORY_SEPARATOR, $file) . $this->___extension;        
-        if (file_exists($___fl) && is_readable($___fl)) {
-            ob_start();
-            include $___fl;            
-            return ob_get_clean();
-        } else {
-            throw new \Exception('View ' . $file . ' cannot be included', 500);
-        }        
-    }
-    
-    public function __set($name, $value) {
-        $this->___data[$name] = $value;
-    }
+	private static $_instance = null;
+	private $___viewPath = null;
+	private $___viewDir = null;
+	private $___data = array();
+	private $___extension = '.php';
+	private $___layoutParts = array();
+	private $___layoutData = array();
+	private function __construct() {
 
-    public function __get($name) {
-        return $this->___data[$name];
-    }
-    
-    /**
-     * 
-     * @return \MVC\View
-     */
-    public static function getInstance() {
-        if (self::$_instance == null) {
-            self::$_instance = new \MVC\View();
-        }
-        return self::$_instance;
-    }
-    
+		$this->___viewPath = \MVC\App::getInstance()->getConfig()->app['viewsDirectory'];
+		if ($this->___viewPath == null) {
+			$this->___viewPath = realpath('../views/');
+		}
+	}
+
+	public function setViewDirectory($path) {
+		$path = trim($path);
+		if ($path) {
+			$path = realpath($path) . DIRECTORY_SEPARATOR;
+			if (is_dir($path) && is_readable($path)) {
+				$this->___viewDir = $path;
+			} else {
+				//todo
+				throw new \Exception('view path', 500);
+			}
+		} else {
+			//todo
+			throw new \Exception('view path', 500);
+		}
+	}
+
+	function getBaseUrl() {
+		// output: /myproject/index.php
+		$currentPath = $_SERVER['PHP_SELF'];
+
+		// output: Array ( [dirname] => /myproject [basename] => index.php [extension] => php [filename] => index )
+		$pathInfo = pathinfo($currentPath);
+
+		// output: localhost
+		$hostName = $_SERVER['HTTP_HOST'];
+
+		// output: http://
+		$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, 5)) == 'https://' ? 'https://' : 'http://';
+
+		// return: http://localhost/myproject/
+		return $protocol . $hostName . $pathInfo['dirname'];
+	}
+
+	public function display($name, $data = array(), $returnAsString = false) {
+
+		if (is_array($data)) {
+			$this->___data = array_merge($this->___data, $data);
+		}
+
+		if (count($this->___layoutParts) > 0) {
+			foreach ($this->___layoutParts as $k => $v) {
+				$r = $this->_includeFile($v);
+				if ($r) {
+					$this->___layoutData[$k] = $r;
+				}
+			}
+		}
+
+		if ($returnAsString) {
+			return $this->_includeFile($name);
+		} else {
+			echo $this->_includeFile($name);
+		}
+	}
+
+	public function getLayoutData($name) {
+		return $this->___layoutData[$name];
+	}
+
+	public function appendToLayout($key, $template) {
+		if ($key && $template) {
+			$this->___layoutParts[$key] = $template;
+		} else {
+			throw new \Exception('Layout ruqire valid key and tepmplate', 500);
+		}
+	}
+	private function _includeFile($file) {
+		if ($this->___viewDir == null) {
+			$this->setViewDirectory($this->___viewPath);
+		}
+		$___fl = $this->___viewDir . str_replace('.', DIRECTORY_SEPARATOR, $file) . $this->___extension;
+		if (file_exists($___fl) && is_readable($___fl)) {
+			ob_start();
+			include $___fl;
+			return ob_get_clean();
+		} else {
+			throw new \Exception('View ' . $file . ' cannot be included', 500);
+		}
+	}
+
+	public function __set($name, $value) {
+		$this->___data[$name] = $value;
+	}
+
+	public function __get($name) {
+		return $this->___data[$name];
+	}
+
+	/**
+	 *
+	 * @return \MVC\View
+	 */
+	public static function getInstance() {
+		if (self::$_instance == null) {
+			self::$_instance = new \MVC\View();
+		}
+		return self::$_instance;
+	}
+
 }
-
-
