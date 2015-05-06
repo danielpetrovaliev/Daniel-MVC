616bacd3a90259db8c8ed758668862ef36706e6e
petrovaliev95 <petrovaliev95@abv.bg>
2015-05-07 00:35:44 +0300

    bug fix

--- trunk/Blog-System/views/post/post.php
+++
@@ -1,4 +0,0 @@
-<?=$this->getLayoutData('header')?>
-<?=$this->getLayoutData('post')?>
-<?=$this->getLayoutData('side-bar')?>
-<?=$this->getLayoutData('footer')?>
--- trunk/README.md
+++
@@ -1,3 +0,0 @@
-# Daniel-MVC
-Simple MVC framework written on PHP for my course "Web Development" in "Software University".
-MVC framework is idea of gatakka.
